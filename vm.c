0315af961930e688f88d8dbb3d66a127372ca68f
bobuhiro11 <blackcherry.mn@gmail.com>
2014-02-24 20:22:16 +0900

    add test

--- vm.c
+++ vm.c
@@ -31,7 +31,7 @@
 #define CODE_RETURN      	0xFF000017
 #define CODE_INVALID 		0xFFFFFFFF
 
-#define VM_DATA_TRUE		0x01		/* for vm_data */
+#define VM_DATA_TRUE		0x01		/* for tag of vm_data */
 #define VM_DATA_FALSE		0x02
 #define VM_DATA_NIL		0x03
 #define VM_DATA_EOF		0x04
@@ -61,6 +61,9 @@ struct vm_data
  * 	CODE_*****
  * operand:
  * 	0x....00 -> number
+ * 	0x..0001 -> true
+ * 	0x..1001 -> false
+ * 	0x..1101 -> false
  * 	0x....11 -> string (char *)
  */
 vm_code code[CODE_MAX];
@@ -100,9 +103,9 @@ static vm_code get_vm_code(const char* s)
 	else if(!strcmp(s,"shift"))		rc =  CODE_SHIFT;
 	else if(!strcmp(s,"apply"))		rc =  CODE_APPLY;
 	else if(!strcmp(s,"return"))		rc =  CODE_RETURN;
-	else if(!strcmp(s,"#t"))		rc =  VM_DATA_TRUE;
-	else if(!strcmp(s,"#f"))		rc =  VM_DATA_FALSE;
-	else if(!strcmp(s,"nil"))		rc =  VM_DATA_NIL;
+	else if(!strcmp(s,"#t"))		rc =  0x00000001;
+	else if(!strcmp(s,"#f"))		rc =  0x00000009;
+	else if(!strcmp(s,"nil"))		rc =  0x0000000d;
 	else{
 		val = strtol(s, &endp, 10);
 		if(*endp == '\0'){ 			/* number */
@@ -253,6 +256,10 @@ struct vm_data exec_code()
 
 	for(;;){
 		printf("pc= %d\n", pc);
+		printf("a=");
+		write_vm_data(a);
+		printf("\n");
+
 		val = code[pc++];
 		switch(val){
 			case CODE_HALT:
@@ -274,6 +281,15 @@ struct vm_data exec_code()
 					val = val >> 2;
 					a.tag = VM_DATA_INTEGER;
 					a.u.integer = val;
+				}else if(code[pc] == 0x00000001){
+					a.tag = VM_DATA_TRUE;
+					pc++;
+				}else if(code[pc]  == 0x00000009){
+					a.tag = VM_DATA_FALSE;
+					pc++;
+				}else if(code[pc] == 0x0000000d){
+					a.tag = VM_DATA_NIL;
+					pc++;
 				}else{
 					/* string */
 					val = code[pc++];
@@ -288,6 +304,9 @@ struct vm_data exec_code()
 			case CODE_BOX:
 				break;
 			case CODE_TEST:
+				val  = code[pc++] >> 2;	/* else adr	*/
+				if(a.tag == VM_DATA_FALSE)
+					pc = val;
 				break;
 			case CODE_PLUS:
 				break;
