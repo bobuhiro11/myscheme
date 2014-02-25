0ca07d5ad0808357bc56870b0b1e676c2405a125
bobuhiro11 <blackcherry.mn@gmail.com>
2014-02-25 17:24:07 +0900

    fix

---
+++ sample/ltak.scm
@@ -0,0 +1,13 @@
+(letrec ([ltak (lambda (x y z)
+                 (if (< (x) (y))
+                   (y)
+                   (if (= (x) (y))
+                     (y)
+                     (ltak (lambda () (ltak (lambda () (- (x) 1)) y z))
+                           (lambda () (ltak (lambda () (- (y) 1)) z x))
+                           (lambda () (ltak (lambda () (- (z) 1)) x y))))))]
+         [tak (lambda (x y z)
+                (ltak (lambda () x)
+                      (lambda () y)
+                      (lambda () z)))])
+  (tak 30 15 0))
